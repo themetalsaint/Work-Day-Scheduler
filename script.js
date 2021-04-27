91f9373e588b4037084eaef3cb165665bfb68e88
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-04-26 20:14:27 -0400

    added js and created divs

---
+++ script.js
@@ -0,0 +1,13 @@
+// TO DO
+// 1. Create Rows with a save button
+// 2. Title has the current Date & Time
+// 3. Each row has a time block 
+// 4. Each row has a save button
+// 5. Each save button saved to Local storage
+
+
+
+
+
+
+
