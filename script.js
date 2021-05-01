dc96af7e6615f2661cfb72e37aa293d3781a8a41
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-04-30 22:15:53 -0400

    added most of js, added css, working on local storage

--- script.js
+++ script.js
@@ -5,14 +5,74 @@
 // 4. Each row has a save button
 // 5. Each save button saved to Local storage
 
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
+
+
+var currentHour = moment().hour(); 
 
 
 
+function hourTracker() {
+
+    //get current number of hours.
+    var currentHour = moment().hour();
+
+$(".block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    console.log( blockHour, currentHour)
+
+    //Blocking out time for past time, the present, and future time
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+})
+}
+
+hourTracker();
+
+
+
+$(document).ready(function () {
+
+//Time Funciton
 $(document).ready(function(){
    
     setInterval(function(){
         $("#date").text(moment().format('h:mm:ss a'));
     }, 1000);
+
+    $(".saveBtn").on("click", function () {
+        
+        console.log(this);
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
+
+        //set items in local storage.
+        localStorage.setItem(time, text);
+        })
+
+    })
 })
 
 
