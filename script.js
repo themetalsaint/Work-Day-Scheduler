d5fec4b4facb316f94000d4d78af9572316ee889
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-04-30 20:17:05 -0400

    simplified timer to use moment.js

--- script.js
+++ script.js
@@ -6,89 +6,14 @@
 // 5. Each save button saved to Local storage
 
 
-// var rightNow = 
-// // Current Day & Time
-// timeDisplayEL.text(rightNow) //wrap in function called displaytime 
-
-// function displayTime(){
-
-// }
 
 
-
-// setInterval(displayTime, 1000)
-
-
-
-
-
-
-// $(document).ready(function() {
-
-var seconds = moment().format("ss")
-var hours = moment().format("h")
-var minutes = moment().format("mm")
-var date = moment().format("MMMM Do YYYY")
-var meridiem = moment().format("a")
-
-
-
-
-$("#hours").html("<span class='show'>" + hours + "<span>");
-$("#minutes").html("<span class='show'>" + minutes + "<span>");
-$("#seconds").html("<span class='show'>" + seconds + "&nbsp <span>");
-// $("#date").html("<span class='show'>" + date + "&nbsp <span>");
-
-
-$("#date").append( date + " " )
-$("#meridiem").append(meridiem)
-
-function currentTime() {
-    if (seconds !== moment().format("ss")){
-        seconds = moment().format("ss");
-        updateHtml("#seconds", seconds)
-    }
-    if (minutes !== moment().format("mm")){
-        minutes = moment().format("mm");
-        updateHtml("#minutes", minutes)
-    }
-    if (hours !== moment().format("h")){
-        hours = moment().format("h");
-        updateHtml("#hours", hours)
-    }
-    // if (date !== moment().format("MMMM Do YYYY")){
-    //     date = moment().format("MMMM Do YYYY");
-    //     updateHtml("#date", date)
-    // }
-    // if (meridiem !== moment().format("a")){
-    //     meridiem = moment().format("a");
-    //     updateHtml("#meridiem", meridiem)
-    // }
-
-    setTimeout(currentTime, 1000)
-}
-
-
-    function updateHtml(id, value){
-        var timeEl= document.querySelector(id)
-
-
-        timeEl.querySelector(".show").className = "hide";
-        var span = document.createElement("span");
-        span.className = "show";
-        span.innerText= value;
-        timeEl.appendChild(span);
-    }
-
-
-
-    currentTime()
-
-
-
-
-// })
-
+$(document).ready(function(){
+   
+    setInterval(function(){
+        $("#date").text(moment().format('h:mm:ss a'));
+    }, 1000);
+})
 
 
 
